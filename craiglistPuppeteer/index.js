a64f1433e76499cd2faa0a89de444b851d6e9bb6
kunalcode12 <kunal818196@gmail.com>
2025-03-31 23:16:32 +0530

    working..

--- craiglistPuppeteer/index.js
+++ craiglistPuppeteer/index.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
 
 async function main() {
   try {
@@ -12,6 +13,19 @@ async function main() {
     await page.type("input#inputPassword", "9899Kun#", { delay: 200 });
 
     page.click("button#login");
+    await page.waitForNavigation();
+
+    await page.goto(
+      "https://accounts.craigslist.org/login/home?show_tab=searches"
+    );
+
+    const content = await page.content();
+    const $ = await cheerio.load(content);
+    console.log(
+      $(
+        "body > article > section > div.account-homepage.account-homepage-saved-searches"
+      ).text()
+    );
   } catch (e) {
     console.error(e);
   }
