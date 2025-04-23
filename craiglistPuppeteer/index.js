47ae7b4eeeef3a08b2cd27e104d188348a3a7582
kunalcode12 <kunal818196@gmail.com>
2025-04-23 18:52:12 +0530

    some changes..

--- craiglistPuppeteer/index.js
+++ craiglistPuppeteer/index.js
@@ -21,11 +21,13 @@ async function main() {
 
     const content = await page.content();
     const $ = await cheerio.load(content);
-    // console.log(
-    //   $(
-    //     "body > article > section > div.account-homepage.account-homepage-saved-searches"
-    //   ).text()
-    // );
+
+    console.log(
+      $(
+        "body > article > section > div.account-homepage.account-homepage-saved-searches"
+      ).text()
+    );
+    console.log("All data fetched successfully");
   } catch (e) {
     console.error(e);
   }
